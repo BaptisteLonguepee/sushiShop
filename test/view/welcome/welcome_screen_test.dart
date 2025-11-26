44bdb83baf06c1c656d977f79e0c791b5195cab6
Baptiste <batbox.longuepee@gmail.com>
2025-11-26 14:43:43 +0100

    refactor(tests): remove unused import from welcome_screen_test.dart

--- test/view/welcome/welcome_screen_test.dart
+++ test/view/welcome/welcome_screen_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sushishop/view/welcome/model/welcome_model.dart';
