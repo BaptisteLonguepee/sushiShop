a7b8d79ab81e3731867f76be88d056ca85e17b08
Baptiste <batbox.longuepee@gmail.com>
2025-11-26 14:39:29 +0100

    test(welcome): add unit and widget tests for WelcomeScreen and ViewModels

--- lib/view/welcome/model/welcome_model.dart
+++ lib/view/welcome/model/welcome_model.dart
@@ -2,6 +2,6 @@ class WelcomeModel {
   final String backgroundImage;
 
   WelcomeModel({
-    this.backgroundImage = 'assets/images/sushi_background.jpg',
+    this.backgroundImage = 'assets/images/sushi_background.png',
   });
 }
