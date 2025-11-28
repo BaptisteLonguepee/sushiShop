31799caed2eb121d08b3789a976695677aa62997
Baptiste <batbox.longuepee@gmail.com>
2025-11-28 10:23:33 +0100

    fix(home): update color opacity method in HomeScreen

--- lib/view/home/view/home_screen.dart
+++ lib/view/home/view/home_screen.dart
@@ -176,7 +176,7 @@ class _ProductCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
+            color: Colors.black.withValues(alpha: 0.1),
             blurRadius: 8,
             offset: const Offset(0, 4),
           ),
@@ -190,7 +190,7 @@ class _ProductCard extends StatelessWidget {
             flex: 3,
             child: Container(
               decoration: BoxDecoration(
-                color: AppColor.cardColor.withOpacity(0.3),
+                color: AppColor.cardColor.withValues(alpha: 0.3),
                 borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(16),
                   topRight: Radius.circular(16),
